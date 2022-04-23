55fb57b7d2c8940b7328c1db561215d29127cc07
sskaren1 <karen.93st@gmail.com>
2022-04-23 01:21:41 -0500

    dia 5

--- semana02/dia5/app/__init__.py
+++ semana02/dia5/app/__init__.py
@@ -1,4 +1,8 @@
+from ensurepip import bootstrap
 from flask import Flask
+
+# importamos bootstrap
+from flask_bootstrap import Bootstrap
 
 #importamos blueprints
 from .admin import admin
@@ -10,6 +14,7 @@ from .config import Config
 def create_app():
     app = Flask(__name__)
 
+    bootstrap = Bootstrap(app)
     app.config.from_object(Config)
 
     app.register_blueprint(admin)
